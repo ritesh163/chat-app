474352eed97e5ee87810797089d17949964603ea
ritesh <riteshyuvasoft163@gmail.com>
2017-08-19 15:47:37 +0530

    updated file

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_to, "/home/ubuntu/chat-app"
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
